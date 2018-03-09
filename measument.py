cd6b4a6b3239e5dca7efe33e1c3dfdaf1fbcea6d
易鑫柯 <andy_yi@dhcp-10-105-31-1.wireless.northwestern.private>
2018-03-09 12:52:58 -0600

    Add list of mesurements including their abbreviations

---
+++ measument.py
@@ -0,0 +1,19 @@
+#Reference of list of Common US Measurements
+#https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement
+#https://en.wikipedia.org/wiki/Cooking_weights_and_measures
+#http://startcooking.com/measurement-and-conversion-charts
+
+
+
+list_of_abbrev_mesurement = {'pn':'pinch','ds':'dash','ssp':'saltspoon','csp':'coffeespoon','fl.dr.':'fluid dram','tsp':'teaspoon',
+	                             't':'teaspoon','C':'cup','pt':'pint','qt':'quart','pot':'pottle','gal':'gallon','fl oz':'fluid ounce',
+	                             'oz':'ounce'}
+
+
+list_of_measurement = ['cup', 'cups', 'ounce', 'ounces', 'tablespoon', 'teaspoon', 'pound', 'pounds',
+	                        'tablespoons','teaspoons','fluid ounce','fluid ounces','quart','quarts','liter','liters',
+	                        'pint','pints','ounce','ounces','gram','grams','gallon','gallons','package','packages',
+	                        'dessertspoon','dessertspoons', 'can','cans', 'needed','dash','pinch','sprig','sprigs'
+                          ]
+
+    
